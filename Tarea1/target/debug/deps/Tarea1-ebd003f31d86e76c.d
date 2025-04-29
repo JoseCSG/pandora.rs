0479e9e051c3a4b1a5231f1785f88b4dd53da1a0
José Carlos Sánchez <a01174050@tec.mx>
2025-04-28 23:58:58 -0600

    reestructura de archivos

--- Tarea1/target/debug/deps/Tarea1-ebd003f31d86e76c.d
+++
@@ -1,9 +0,0 @@
-C:\Users\jcsg6\Documentos\Uni\OctavoSemestre\Aplicaciones (IA y Compi)\pandora\Tarea1\target\debug\deps\libTarea1-ebd003f31d86e76c.rmeta: src\main.rs src\utils\mod.rs src\utils\node.rs src\utils\queue.rs src\utils\stack.rs
-
-C:\Users\jcsg6\Documentos\Uni\OctavoSemestre\Aplicaciones (IA y Compi)\pandora\Tarea1\target\debug\deps\Tarea1-ebd003f31d86e76c.d: src\main.rs src\utils\mod.rs src\utils\node.rs src\utils\queue.rs src\utils\stack.rs
-
-src\main.rs:
-src\utils\mod.rs:
-src\utils\node.rs:
-src\utils\queue.rs:
-src\utils\stack.rs:
--- Tarea1/target/debug/deps/Tarea1-f8e4532175aeae48.d
+++
@@ -1,9 +0,0 @@
-C:\Users\jcsg6\Documentos\Uni\OctavoSemestre\Aplicaciones (IA y Compi)\pandora\Tarea1\target\debug\deps\libTarea1-f8e4532175aeae48.rmeta: src\main.rs src\utils\mod.rs src\utils\node.rs src\utils\queue.rs src\utils\stack.rs
-
-C:\Users\jcsg6\Documentos\Uni\OctavoSemestre\Aplicaciones (IA y Compi)\pandora\Tarea1\target\debug\deps\Tarea1-f8e4532175aeae48.d: src\main.rs src\utils\mod.rs src\utils\node.rs src\utils\queue.rs src\utils\stack.rs
-
-src\main.rs:
-src\utils\mod.rs:
-src\utils\node.rs:
-src\utils\queue.rs:
-src\utils\stack.rs:
